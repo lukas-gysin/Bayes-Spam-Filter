edc73db3df4ad23ccfc7a54d781a030668d417c6
lukas-gysin <lukas.gysin@students.fhnw.ch>
2021-10-22 16:58:33 +0200

    feat: Nice Printout

--- src/main/java/ch/fhnw/dist/bayes/Bayes.java
+++ src/main/java/ch/fhnw/dist/bayes/Bayes.java
@@ -10,32 +10,53 @@ import java.util.zip.ZipFile;
  */
 public class Bayes {
 
-  public static final Filter bayes = new Filter(1);
+  private static final Filter bayes = new Filter(1);
+  private static int correctMails = 0;
+  private static int allMails = 0;
 
   public static void main(String[] args) {
     bayes.train("src/main/resources/ham-anlern.zip", "src/main/resources/spam-anlern.zip");
+
+    categoriseZip("src/main/resources/ham-test.zip", false);
+    categoriseZip("src/main/resources/spam-test.zip", true);
+
+    System.out.println("=== Bayes Spam Filter von Lukas Gysin ===");
+    System.out.println("Schwellwert:\t" + bayes.getThreshold());
+    System.out.println("Alpha:\t\t\t" + bayes.getAlpha());
+    System.out.println("Erkennungsrate:\t" + (double)correctMails/allMails);
+
+    correctMails = 0;
+    allMails = 0;
+
     bayes.calibrate("src/main/resources/ham-kallibrierung.zip", "src/main/resources/spam-kallibrierung.zip");
+
+    categoriseZip("src/main/resources/ham-test.zip", false);
+    categoriseZip("src/main/resources/spam-test.zip", true);
+
+    System.out.println("------ Werte nach der Kalibrierung ------");
+    System.out.println("Schwellwert:\t" + bayes.getThreshold());
+    System.out.println("Alpha:\t\t\t" + bayes.getAlpha());
+    System.out.println("Erkennungsrate:\t" + ((double)correctMails/allMails));
   }
 
-  private static void categorizeMail(BufferedReader mail, boolean spam) {
-    double spamProbability;
-    double numerator = 1;
-    double denominator = 1;
+  public static void categoriseZip(String path, boolean spam){
     try {
-      while (mail.ready()){
-        for (String word : mail.readLine().split(" ")) {  // Split the line into words, separated by a whitespace
-          if (!word.isBlank()){
-            numerator *= bayes.spamProbability(word);
-            denominator *= bayes.hamProbability(word);
-          }
+      ZipFile zipFile = new ZipFile(path);
+      zipFile.stream().forEach(mail -> {  // Read each mail in the `.zip`-file
+        try {
+          BufferedReader content = new BufferedReader(new InputStreamReader(zipFile.getInputStream(mail)));
+          // When the mail is categorized correct, the `correctMail`-counter should be increase by 1
+          correctMails += (bayes.categorize(content) == spam ? 1 : 0);
+          allMails++;
+          content.close();
+        } catch (IOException e) {
+          // Couldn't get the InputStream for the ZipEntity
+          e.printStackTrace();
         }
-      }
-      denominator += numerator;
-      spamProbability = numerator / denominator;
-      System.out.println("Spam probability: " + spamProbability);
-      System.in.read();
+      });
+      zipFile.close();
     } catch (IOException e) {
-      // An error occurred on the reading process
+      // Couldn't find the `.zip`-file
       e.printStackTrace();
     }
   }
